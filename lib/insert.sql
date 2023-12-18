6c6cbb34bdcf3cd576ee8dd8f919cef41723a78d
robin nganga smith <saintofcookies.2021@gmail.com>
2023-12-18 09:27:01 +0300

    Completed assignemnt

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+  (1, 'Mr. Chocolate', 5, 'M', 'brown', 'calm', true),
+  (2, 'Rowdy', 3, 'M', 'black', 'aggressive', true),
+  (3, 'Tabitha', 2, 'F', 'white', 'friendly', true),
+  (4, 'Sergeant Brown', 7, 'M', 'brown', 'calm', true),
+  (5, 'Melissa', 4, 'F', 'black', 'friendly', true),
+  (6, 'Grinch', 1, 'M', 'green', 'grumpy', true),
+  (7, 'Wendy', 6, 'F', 'brown', 'friendly', true),
+  (8, 'unnamed', null, null, null, null, null);
